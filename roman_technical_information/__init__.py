ea7221799caa846ed698c83ea75bbec61637778e
rzellem <robert.t.zellem@nasa.gov>
2026-05-22 17:33:26 -0400

    Update to fix version issue

--- roman_technical_information/__init__.py
+++ roman_technical_information/__init__.py
@@ -1,10 +1,14 @@
 from pathlib import Path
 from importlib.resources import files
+import importlib.metadata
 import yaml
 from astropy.table import Table, QTable
 
-
-_version_file = Path(__file__).parent.parent / "VERSION.md"
-__version__ = _version_file.read_text(encoding="utf-8").strip()
+try:
+    # This reads the version directly from the pip metadata created by pyproject.toml
+    __version__ = importlib.metadata.version("roman-technical-information")
+except importlib.metadata.PackageNotFoundError:
+    # Fallback if someone runs the code without installing it
+    __version__ = "unknown"
 
 PACKAGEDIR = files("roman_technical_information") / "data"
