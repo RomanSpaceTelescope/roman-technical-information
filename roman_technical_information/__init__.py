5752605cfbb4774eb740299a86ebd21619f32489
rzellem <robert.t.zellem@nasa.gov>
2026-05-22 17:00:49 -0400

    Addressed Christina Hedges' feedback.

--- roman_technical_information/__init__.py
+++ roman_technical_information/__init__.py
@@ -1,4 +1,10 @@
 from pathlib import Path
+from importlib.resources import files
+import yaml
+from astropy.table import Table, QTable
+
 
 _version_file = Path(__file__).parent.parent / "VERSION.md"
 __version__ = _version_file.read_text(encoding="utf-8").strip()
+
+PACKAGEDIR = files("roman_technical_information") / "data"
--- roman_technical_information/io.py
+++ roman_technical_information/io.py
@@ -1,17 +1,11 @@
-from importlib.resources import files
-import yaml
-from astropy.table import Table, QTable
-
-BASE = files("roman_technical_information") / "data"
-
 def open_data(relative_path: str):
     """Return a file handle for a data file inside roman_technical_information/data."""
-    return (BASE / relative_path).open("r")
+    return (PACKAGEDIR / relative_path).open("r")
 
 def load_yaml(relative_path: str):
-    with open_data(relative_path) as f:
+    with open_data(PACKAGEDIR / relative_path) as f:
         return yaml.safe_load(f)
 
-def load_table(relative_path: str, table_class=Table):
-    path = BASE / relative_path
-    return table_class.read(path, format="ascii.ecsv")
+def load_table(relative_path: str) -> Table:
+    path = PACKAGEDIR / relative_path
+    return Table.read(path, format="ascii.ecsv")
